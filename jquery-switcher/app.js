7f8a679a151c49c2ed57015dc57117dc70c2f401
ThibaultDBVLL <thibault.daboville@gmail.com>
2017-01-08 19:17:17 +0100

    travail rendu

---
+++ jquery-switcher/app.js
@@ -0,0 +1,32 @@
+var species = {
+	cat    : "cat.jpg",
+	bear   : "bear.jpg",
+	fish   : "fish.jpg"
+};
+
+function main(){
+	/*
+	tout votre code doit se trouver dans cette fonction,
+	vous pouvez biensur créer d'autres fonctions si nécessaire
+	*/
+
+	// 1. placez un listener sur le click des éléments <button>
+	// 2. dans le callback récupérez le data attribute animal
+	// 3. récupérerez dans l'objet species la valeur correspond à l'attribut récupéré
+	// 4. ajoutez élément img en assignant à son attibut src la valeur précédemment récupérée
+	// 5. inserez cet élément dans l'élément ayant l'id holder
+	//
+	// Conseils : avancez étape par étape, console.log et debugger autant que nécessaires !
+
+	$('.ui button').click (function(){
+		var animal = $(this).attr("data-animal");
+
+		$('#holder').html("<img src='img/"+species[animal]+"'>");
+
+	});
+}
+
+
+$(document).ready(function(){
+	main();
+});
